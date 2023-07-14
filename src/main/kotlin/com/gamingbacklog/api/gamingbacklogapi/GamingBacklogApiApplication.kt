1010bf361e02e53d1ba6268505f320521524ccd4
Tracy Goldman <tag96@cornell.edu>
2023-07-14 10:40:55 -0400

    Delete API and cors

--- src/main/kotlin/com/gamingbacklog/api/gamingbacklogapi/GamingBacklogApiApplication.kt
+++ src/main/kotlin/com/gamingbacklog/api/gamingbacklogapi/GamingBacklogApiApplication.kt
@@ -1,7 +1,5 @@
 package com.gamingbacklog.api.gamingbacklogapi
 
-import com.gamingbacklog.api.gamingbacklogapi.repositories.LibraryRepository
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
