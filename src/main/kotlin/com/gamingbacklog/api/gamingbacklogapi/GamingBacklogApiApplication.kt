519025a6e4b6285cf784f06174b74d72420874b8
Tracy Goldman <tag96@cornell.edu>
2023-12-11 22:10:19 -0500

    Global Cors

--- src/main/kotlin/com/gamingbacklog/api/gamingbacklogapi/GamingBacklogApiApplication.kt
+++ src/main/kotlin/com/gamingbacklog/api/gamingbacklogapi/GamingBacklogApiApplication.kt
@@ -2,10 +2,30 @@ package com.gamingbacklog.api.gamingbacklogapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
 class GamingBacklogApiApplication
 
 fun main(args: Array<String>) {
 	runApplication<GamingBacklogApiApplication>(*args)
+}
+
+@Configuration
+class CorsConfig {
+
+  @Bean
+  fun corsConfigurer(): WebMvcConfigurer {
+    return object : WebMvcConfigurer {
+      override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+          .allowedOrigins("http://localhost:3000")
+          .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+          .allowedHeaders("*")
+      }
+    }
+  }
 }
